acce9ff2d7cf7bec4709b9cce289b7d26db742f8
zouhaochen <zouhaochenluke@gmail.com>
2024-01-29 22:32:34 +0800

    graph setting create

--- graph_setting.py
+++ graph_setting.py
@@ -25,6 +25,50 @@ def get_graph_dot_obj(graph_spec) -> List[Dot]:
     return graph_spec
 
 
+def get_edges(graph_spec, label=False):
+    graphs = get_graph_dot_obj(graph_spec)
+    n_graphs = len(graphs)
+    if n_graphs > 1:
+        return [get_edges(graph) for graph in graphs]
+    elif n_graphs == 0:
+        raise ValueError(f"Your input had no graphs")
+    else:
+        graph = graphs[0]
+        edges = graph.get_edges()
+        edges_list = []
+        if not label:
+            for edge in edges:
+                r1, r2 = graph.get_node(edge.get_source())[0].get_label().strip('\"').strip('\\').strip('\"'), \
+                         graph.get_node(edge.get_destination())[0].get_label().strip('\"').strip('\\').strip('\"')
+                if '/' in r1:
+                    r1 = r1.split('/')[1]
+                elif '\\' in r1:
+                    r1 = r1.split('\\')[0]
 
+                if '/' in r2:
+                    r2 = r2.split('/')[1]
+                elif '\\' in r1:
+                    r2 = r2.split('\\')[0]
+
+                edges_list.append([r1, r2])
+        else:
+            for edge in edges:
+                r1, r2, r3 = graph.get_node(edge.get_source())[0].get_label().strip('\"').strip('\\').strip('\"'), \
+                             graph.get_node(edge.get_destination())[0].get_label().strip('\"').strip('\\').strip(
+                                 '\"'), edge.get_label().strip('\"')[1:]
+                if '/' in r1:
+                    r1 = r1.split('/')[1]
+                elif '\\' in r1:
+                    r1 = r1.split('\\')[0]
+
+                if '/' in r2:
+                    r2 = r2.split('/')[1]
+                elif '\\' in r1:
+                    print("called")
+                    r2 = r2.split('\\')[0]
+
+                edges_list.append([r1, r2, r3])
+
+        return edges_list
 
 
