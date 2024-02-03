cacea6ebdf3c7a348cd092f5395535a524d7e52a
zouhaochen <zouhaochenluke@gmail.com>
2024-02-03 11:37:49 +0800

    config update

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ import torch
 TRAIN_FILE_PATH = './output/process/fine_grained_sentiment_analysis.train.csv'
 TEST_FILE_PATH = './output/process/fine_grained_sentiment_analysis.test.csv'
 
-
 BIO_O_ID = 0
 BIO_B_ID = 1
 BIO_I_ID = 2
 BIO_MAP = {'O': BIO_O_ID, 'B-ASP': BIO_B_ID, 'I-ASP': BIO_I_ID}
-
 
 ENT_SIZE = 3
 
